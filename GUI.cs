35f62a8be2c93db712a0286ad4deac2be4b3d93d
Nikongen <nikolaslongen@gmail.com>
2015-08-03 11:19:34 +0200

    Parent
    GPIO Test

---
+++ GUI.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BollerwagenSharp
+{
+    public partial class GUI : Form
+    {
+        c_GPIO GPIO;
+        
+        public GUI()
+        {
+            InitializeComponent();
+            GPIO = new c_GPIO();
+        }
+
+        private void GUI_Load(object sender, EventArgs e)
+        {
+            //this.FormBorderStyle = FormBorderStyle.None; // Rahmen ausblenden
+            //this.TopMost = true; // Formular ganz im Vordergrund, also auch vor der Taskleiste anzeigen
+            //this.Bounds = Screen.PrimaryScreen.Bounds; // Formulargröße auf Größe des Bildschirms festlegen
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = GPIO.schalter();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            GPIO.an();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GPIO.aus();
+        }
+
+
+    }
+}
---
+++ c_GPIO.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RaspberryPiDotNet;
+using System.Windows.Forms;
+
+namespace BollerwagenSharp
+{
+    class c_GPIO
+    {
+        private GPIOMem gpio17;
+        private GPIOMem gpio24;
+        int i = 0;
+
+        public c_GPIO()
+        {
+
+
+        }
+
+        ~c_GPIO() 
+        {
+        }
+
+
+        public string schalter()
+        {
+            if (gpio24 == null)
+            {
+                gpio24 = new GPIOMem(GPIOPins.V2_GPIO_24);
+                gpio24.PinDirection = GPIODirection.In;
+            }
+            return gpio24.Read().ToString();
+         }
+
+        public void an()
+        {
+            if (gpio17 == null)
+            {
+                gpio17 = new GPIOMem(GPIOPins.V2_GPIO_17);
+            }
+            gpio17.Write(PinState.High);
+
+        }
+
+        public void aus()
+        {
+            if (gpio17 == null)
+            {
+                gpio17 = new GPIOMem(GPIOPins.V2_GPIO_17);
+            }
+            gpio17.Write(PinState.Low);
+        }
+
+        public void test()
+        {
+            GPIOMem test;
+
+            switch (i)
+            {
+                case 0: test = new GPIOMem(GPIOPins.GPIO_17);
+                    test.Write(PinState.High);
+                    test.Dispose();
+                    break;
+                case 2: test = new GPIOMem(GPIOPins.V2_GPIO_17);
+                    test.Write(PinState.High);
+                    test.Dispose();
+                    break;
+               
+
+            }
+            
+                
+        
+        }
+    }
+    
+}
